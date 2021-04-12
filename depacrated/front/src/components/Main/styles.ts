c3270dee7cb6c9a483f972250e081139f634af6a
mmartiniano <luscasmm@outlook.com>
2021-04-12 14:23:33 -0300

    removed depracted folder

--- depacrated/front/src/components/Main/styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const Container = styled.div`
-    min-width: 100%;
-    min-height: calc(100vh - 3.5rem);
-    display: flex;
-    justify-content: center;
-    align-items: center;
-`;
--- depacrated/front/src/components/Thumbnail/styles.ts
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-
-export const Image = styled.img`
-    width: 100%;
-    border-radius: ${props => props.theme.radius};
-`;
